33faa3b2c83474ecb2eee5729702101a225b3a3f
Jacopo Ventura <jacopo.ventura.87@gmail.com>
2024-01-10 12:34:33 +0100

    changed file path

--- asset_analysis_main.py
+++ asset_analysis_main.py
@@ -12,7 +12,7 @@ pandas.set_option('display.max_columns', 10)
 pandas.options.display.float_format = '{:,.1f}'.format
 
 TICKER = "SPY"
-PATH_TO_REPORT = '~/Desktop/Trading/'
+PATH_TO_REPORT = '~/Desktop/projects/Trading/'
 
 
 # Press the green button in the gutter to run the script.
@@ -21,8 +21,8 @@ if __name__ == '__main__':
     t0 = timeit.default_timer()
 
     # Query data from yahoo finance
-    start = datetime.datetime(2022, 9, 1)  # Year, Month, Day
-    end = datetime.datetime(2023, 7, 20)  # Year, Month, Day
+    start = datetime.datetime(2022, 1, 1)  # Year, Month, Day
+    end = datetime.datetime(2022, 12, 20)  # Year, Month, Day
     spy = PriceAnalysis(TICKER, start, end, PATH_TO_REPORT)
     spy.run()
     elapsed = timeit.default_timer() - t0
