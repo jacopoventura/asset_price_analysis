eb5e5da1ba8adb443a4316b92f5e03eb29c83f47
Jacopo <31012859+jacopoventura@users.noreply.github.com>
2024-08-07 18:00:06 +0200

    Update about_this_app.py

--- pages/about_this_app.py
+++ pages/about_this_app.py
@@ -4,4 +4,8 @@ st.markdown('Copyright (c) Jacopo Ventura, 2024. Distribution not allowed.', uns
 
 st.markdown('<h2>About this app</h2>', unsafe_allow_html=True)
 
-st.markdown('This application analyzes the price movement of assets in a specific time window. The output is a detailed report of the probabilities of the price movement up to numerous price change levels.', unsafe_allow_html=True) 
+st.markdown('This application analyzes the price movement of assets in a specific time window. The output is a detailed report of the probabilities of the price movement up to numerous price change levels.', unsafe_allow_html=True)
+
+st.markdown('When trading with options, the greek *delta* provides an empirical estimate of the trade success probaiblity. However, unexpected volatility can occur just right after the trade open, with the underlying price going in the opposite trade direction.
+When this happens, it's crucial for the trader to stay calm and handle the trade correctly.
+This is possible by knowing the probability of the price change of the underlying asset. With this data, better strike levels can be chosen.', unsafe_allow_html=True)
