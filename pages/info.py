e7bdcc08e278271f85c6c57cc5a95a24967b7b5e
Jacopo <31012859+jacopoventura@users.noreply.github.com>
2024-08-07 17:45:02 +0200

    Update info.py

--- pages/info.py
+++ pages/info.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 st.markdown('Copyright (c) Jacopo Ventura, 2024. Distribution not allowed.', unsafe_allow_html=True)
 
-st.markdown('<h2>TEST</h2>', unsafe_allow_html=True)
+st.markdown('<h2>What the app does</h2>', unsafe_allow_html=True)
+
+st.markdown('This application analyzes the price movement of assets in a specific time window. The output is a detailed report of the probabilities of the price movement up to numerous price change levels.', unsafe_allow_html=True) 
