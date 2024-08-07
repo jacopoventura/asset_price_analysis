89ab8235e961ecd8284febd520f66e1d30f7e1a2
Jacopo <31012859+jacopoventura@users.noreply.github.com>
2024-08-07 15:05:12 +0200

    Update Analyze.py

--- Analyze.py
+++ Analyze.py
@@ -62,11 +62,11 @@ if __name__ == '__main__':
 
 hide_streamlit_style = """
             <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
             </style>
             """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown(
     """
