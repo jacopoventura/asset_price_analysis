0b51d561d7bd19ed98e396034640ec393a90bd55
Jacopo <31012859+jacopoventura@users.noreply.github.com>
2024-08-07 17:24:52 +0200

    Update info.py

--- pages/info.py
+++ pages/info.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.markdown('Copyright (c) Jacopo Ventura, 2024. Distribution not allowed.', unsafe_allow_html=True)
 
-st.markdown('#TEST', unsafe_allow_html=True)
+st.markdown('<h2>TEST</h2>', unsafe_allow_html=True)
