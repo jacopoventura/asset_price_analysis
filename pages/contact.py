0544f3755c6aa0ba7d311296e8bff424d598a409
Jacopo <31012859+jacopoventura@users.noreply.github.com>
2024-08-07 17:00:00 +0200

    Update contact.py

--- pages/contact.py
+++ pages/contact.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.sidebar.markdown('For suggestions, requests and collaborations write to: <a href="mailto:hello@streamlit.io">jacopo.ventura.87@gmail.com</a>', unsafe_allow_html=True)
+st.markdown('For suggestions, requests and collaborations write to: <a href="mailto:hello@streamlit.io">jacopo.ventura.87@gmail.com</a>', unsafe_allow_html=True)
