67f3d2530f52a304d30aeefdaf32a454aa0ada77
Jacopo <31012859+jacopoventura@users.noreply.github.com>
2024-08-07 14:33:33 +0200

    Update Analyze.py

--- Analyze.py
+++ Analyze.py
@@ -18,12 +18,18 @@ PATH_TO_REPORT = '~/Desktop/projects/Trading/'
 path_to_html = "./"
 
 # Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # Your app code goes here
 
